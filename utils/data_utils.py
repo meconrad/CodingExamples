eddad93ea665741c1477d7c52bf03eaa8190a5ba
Mollie Conrad <meconrad2@gmail.com>
2026-04-28 23:00:16 -0400

    Add GMM associated utilities

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,45 +1,67 @@
 import numpy as np
-import scipy.stats as stats
 import random
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+from utils.plotting_utils import plot_gaussian_mixture_data
+
 
 random.seed(45)
 
 
 def create_multi_modal_test_data(
+        loc_range: tuple = (0, 100),
+        std_range: tuple = (1, 5),
+        size_range: tuple = (500, 2000),
         n_modes: int = 3,
         noise_std: float = 5.0,
         noise_size: int = 100,
+        show_fig: bool = True,
 ):
     """ 
     Generate fake test data with multiple modes.
     
     Parameters
     ----------
-    n_modes: int
-        the number of modes to generate, default = 3
+    loc_range : tuple, default = (0, 100)
+        (minimum dataset value, maximum dataset value)
 
-    noise_std : float
-        standard deviation of noise, default = 5.0
+    std_range : tuple, default = (1, 5)
+        (minimum std dev value, maximum std dev value)
 
-    noise_size : int
-        number of "noisy" datapoints to add to distribution, default = 100
+    size_range : tuple, default = (500, 2000)
+        (minimum loc value, maximum loc value)
+
+    n_modes : int, default = 3
+        the number of modes to generate
+
+    noise_std : float, default = 5.0
+        standard deviation of noise
+
+    noise_size : int, default = 100
+        number of "noisy" datapoints to add to distribution
+
+    show_fig : bool, default = True
+        plot multimodal data and individual modes on figure
 
     Returns
     -------
     data : list
         shuffled list of multi-modal datapoints
 
-    modes: list of list
+    modes : list of list
         multi-modal datasets individualized
+
+    fig : matplotlib.figure.Figure
 
     """
     modes = []
-    # for each mode requested, randomly choose a location, scale, and dataset size
+    # For each mode requested, randomly choose a location, scale, and dataset size
     # then add noise, add dataset to list of modes
     for _ in range(n_modes):
-        loc = random.randint(0, 30)
-        data_std = random.randint(1, 5)
-        data_size = random.randint(500, 2000)
+        loc = random.randint(*loc_range)
+        data_std = random.randint(*std_range)
+        data_size = random.randint(*size_range)
         mode_data = (
             list(stats.norm.rvs(loc=loc, scale=data_std, size=data_size)) + 
             list(stats.norm.rvs(loc=loc, scale=noise_std, size=noise_size))
@@ -50,7 +72,12 @@ def create_multi_modal_test_data(
     data = modes[0] + modes[1] + modes[2]
     random.shuffle(data)
 
-    return data, modes
+    # Generate and show results on a figure if requested
+    fig = None
+    if show_fig is True:
+        fig = plot_gaussian_mixture_data(data, modes)
+
+    return data, modes, fig
 
 
 def create_test_data(
@@ -62,11 +89,11 @@ def create_test_data(
     
     Parameters
     ----------
-    noise_std : float
-        standard deviation of noise, default = 5.0
+    noise_std : float, default = 5.0
+        standard deviation of noise
 
-    noise_size : int
-        number of "noisy" datapoints to add to distribution, default = 100
+    noise_size : int, default = 100
+        number of "noisy" datapoints to add to distribution
 
     Returns
     -------
@@ -91,6 +118,7 @@ def create_test_data(
         "triang": list(stats.triang.rvs(c=1.0, loc=1.5, scale=2.0, size=1000)) + list(stats.norm.rvs(loc=0, scale=noise_std, size=noise_size)),
     }
 
+    # Shuffle the noise in with the datasets so noise is more natural
     for dataset in data.values():
         random.shuffle(dataset) 
 
--- utils/plotting_utils.py
+++ utils/plotting_utils.py
@@ -1,5 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from cycler import cycler
+
+
+# Define accessible colors (e.g., Wong palette)
+cb_colors = ["#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"]
+
+# Create a cycler that changes both color and linestyle
+custom_cycler = (cycler(color=cb_colors) + 
+                 cycler(linestyle=["-", "--", ":", "-.", "-", "--", ":"]))
 
 
 def create_multi_fig_with_stats(
@@ -23,7 +32,7 @@ def create_multi_fig_with_stats(
     plot_type : str
         ["hist", "scatter"]
 
-    show_fig : bool
+    show_fig : bool, default = True
         Show the figure when running the function
 
     Returns
@@ -33,27 +42,140 @@ def create_multi_fig_with_stats(
     """
     ncols = 2
     nrows = len(input_data) // ncols
+    # auto-scale figure based on the number of rows and cols needed
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(7*ncols, 3*nrows))
     for i, (ax, (variable, dataset)) in enumerate(zip(axes.flatten(), input_data.items())):
         ax.grid(True, alpha=0.5)
         match plot_type:
             case "hist":
-                ax.hist(dataset, bins=100, color='darkgrey')
-                ax.axvline(np.median(dataset), color='k', ls='--', lw=1, label="median")
-                ax.axvline(np.mean(dataset), color='#D55E00', ls='--', lw=1, label="mean")
-                ax.axvline(np.mean(dataset) + 3.0 * np.std(dataset), color='g', ls='--', lw=1, label=r"+3$\sigma$")
-                ax.axvline(np.mean(dataset) - 3.0 * np.std(dataset), color='b', ls='--', lw=1, label=r"-3$\sigma$")
+                # plot the histogram and the associated stats as vertical lines
+                ax.hist(dataset, bins=100, color="darkgrey")
+                ax.axvline(np.median(dataset), color="k", ls="--", lw=1, label="median")
+                ax.axvline(np.mean(dataset), color="#D55E00", ls="--", lw=1, label="mean")
+                ax.axvline(np.mean(dataset) + 3.0 * np.std(dataset), color="g", ls="--", lw=1, label=r"+3$\sigma$")
+                ax.axvline(np.mean(dataset) - 3.0 * np.std(dataset), color="b", ls="--", lw=1, label=r"-3$\sigma$")
+
             case "scatter":
-                ax.plot(dataset, 'o', ms=1, color='darkgrey')
-                ax.axhline(np.median(dataset), color='k', ls='--', lw=1, label="median")
-                ax.axhline(np.mean(dataset), color='#D55E00', ls='--', lw=1, label="mean")
-                ax.axhline(np.mean(dataset) + 3.0 * np.std(dataset), color='g', ls='--', lw=1, label=r"+3$\sigma$")
-                ax.axhline(np.mean(dataset) - 3.0 * np.std(dataset), color='b', ls='--', lw=1, label=r"-3$\sigma$")
+                # plot the scatterplot and the associated stats as horizontal lines
+                ax.plot(dataset, "o", ms=1, color="darkgrey")
+                ax.axhline(np.median(dataset), color="k", ls="--", lw=1, label="median")
+                ax.axhline(np.mean(dataset), color="#D55E00", ls="--", lw=1, label="mean")
+                ax.axhline(np.mean(dataset) + 3.0 * np.std(dataset), color="g", ls="--", lw=1, label=r"+3$\sigma$")
+                ax.axhline(np.mean(dataset) - 3.0 * np.std(dataset), color="b", ls="--", lw=1, label=r"-3$\sigma$")
 
         ax.set_title(variable)
+        # Allow only one legend and move outside of the axes frame
         if i == 1:
             ax.legend(loc=2, bbox_to_anchor=(1.05, 1.05))
     plt.tight_layout()
     if show_fig is True:
         plt.show()
     return fig
+
+
+def plot_gaussian_mixture_data(
+        data: list,
+        modes: list
+        ):
+    """ 
+    Plot the multi-modal dataset as well as the individual components.
+
+    Parameters
+    ----------
+    data : list
+        list of datapoints representing the combined components
+
+    modes : list of list
+        list of list of datapoints
+
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+    
+    """
+    # Set up the figure
+    fig = plt.figure(figsize=(7, 3))
+    plt.gca().set_prop_cycle(custom_cycler)
+    plt.grid(True, alpha=0.4)
+    # Add the multimodal dataset
+    plt.hist(data, bins=100, label="Multimodal Data")
+    # Add the components
+    for i, _ in enumerate(modes):
+        plt.hist(modes[i], bins=30, alpha=0.5, label=f"Component {i}")
+    plt.legend()
+    plt.title("Original Generated Multimodal Dataset")
+    plt.figtext(
+        0.5,
+        0.005,
+        "*Note: modal data might not fit multimodal dataset exactly due to bin size differences",
+        ha="center",
+        fontsize=8,
+        style="italic"
+        )
+    plt.show()
+    return fig
+
+
+def plot_gmm_results(
+        X: np.array,
+        gmm_results: dict,
+        component: int,
+        upper: np.array,
+        lower: np.array
+):
+    """ 
+    Plot the results of utils.modelling_utils.identify_multi_modes_in_dataset, including the fitted
+    dataset, the total multimodal PDF, the fitted components, and the 3-sigma regions of each component.
+
+    Parameters
+    ----------
+    X : np.array
+        numpy array of datapoints
+    
+    gmm_results : dict of dict
+        output from Gaussian Mixture Model implemented in utils.modelling_utils.identify_multi_modes_in_dataset
+
+    component : int
+        the component to be plotted, usually the best fit component
+
+    upper : np.array
+        the mean + 3 sigma threshold of each component
+
+    lower : np.array
+        the mean - 3 sigma threshold of each component
+
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+
+    """
+    # Define range for plotting
+    x = np.linspace(X.min()-2, X.max()+2, 1000).reshape(-1, 1)
+
+    # Set up figure
+    fig = plt.figure(figsize=(7, 3))
+    plt.gca().set_prop_cycle(custom_cycler)
+    plt.grid(True, alpha=0.4)
+
+    # Add 3 sigma span regions
+    for color, (i, _) in zip(custom_cycler, enumerate(gmm_results[component]["pdf_components"])):
+        plt.axvspan(lower[i], upper[i], alpha=0.4, color=color["color"], label=rf"3$\sigma$ Region {i}")
+
+    # Add multimodal dataset as histogram and PDF
+    plt.hist(X, bins=100, density=True, color="darkgrey", label="Data")
+    plt.plot(x, gmm_results[component]["pdf_total"], "-k", label="Total GMM PDF", linewidth=2)
+
+    # Add the GMM fitted components
+    for color, (i, _) in zip(custom_cycler, enumerate(gmm_results[component]["pdf_components"])):
+        plt.plot(x, gmm_results[component]["pdf_components"][i], c=color["color"], ls="--", label=f"Component {i}")
+        
+    plt.legend(loc=2, bbox_to_anchor=(1.05, 1.0))
+    plt.title(r"GMM Fitted Components, Total PDF, and 3$\sigma$ Thresholds")
+    plt.text(
+        np.min(x)*1.1,
+        np.max(gmm_results[component]["pdf_total"])*0.9,
+        f"Log Likelihood = {gmm_results[component]["log_likelihood"]}"
+        )
+    plt.show()
+    return fig
+
